63d0c387ae95b7a4323d370a7220f40b5c82e730
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2023-12-02 10:48:30 +0000

    [stop]レイアウトの変更

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,12 +5,4 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :opinion, presence: true
   
-  def get_image(width, height)
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no-image.jpg')
-      image.attach(io: File.open(file_path), filename: 'no-image.jpg', content_type: 'image/jpeg')
-    end
-    image.variant(resize_to_limit: [width, height]).processed
-  end
-
 end
