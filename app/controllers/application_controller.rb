e449c5e5cc50deea381da06ea6c8e371c9f21a94
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2023-12-02 11:13:51 +0000

    [stop]レイアウトheaderのabout遷移先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
+  skip_before_action :authenticate_user!, only: [:about]
   
   def after_sign_in_path_for(resource)
     books_path
